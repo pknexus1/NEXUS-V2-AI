aba383f28bc2217c9d705e564abb03df3e2ab527
Pkdriller <nexuspk04@gmail.com>
2025-08-15 22:07:31 +0300

    Update index.js

--- index.js
+++ index.js
@@ -124,6 +124,14 @@ const port = process.env.PORT || 9090;
 │ ★   https://whatsapp.com/channel/0029VatOy2EAzNc2WcShQw1j    
 ╰─🔥 *Powered by Pkdriller*`;
     conn.sendMessage(conn.user.id, { image: { url: `https://i.postimg.cc/SR9S0ZdT/11900809-f4ad-46ee-bedf-f430eed4bab8.jpg` }, caption: up })
+	  caption: up,
+        contextInfo: {
+            forwardingScore: 1,
+            isForwarded: true,
+            forwardedNewsletterMessageInfo: {
+                newsletterJid: "120363297417334452@newsletter", // jid ya channel yako
+                newsletterName: "NEXUS UPDATES", // jina la channel
+                serverMessageId: 1
   }
   })
   conn.ev.on('creds.update', saveCreds)
